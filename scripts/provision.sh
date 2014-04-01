1864ad3c5836f74b1fa47cdbf98b8ff285e5e05b
Kane See <ksee@inferlink.com>
2014-04-01 15:55:11 -0700

    updated scripts

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-echo 'inference' | sudo -S apt-get update
-echo 'inference' | sudo -S apt-get install -y puppet
+sudo apt-get update
+sudo apt-get install -y puppet
 
 # for Apache
-echo 'inference' | sudo -S apt-get install -y libapr1 libaprutil1 libaprutil1-dbd-mysql php5-mysql
+sudo apt-get install -y libapr1 libaprutil1 libaprutil1-dbd-mysql php5-mysql
